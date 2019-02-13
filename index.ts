74630571826069a28265e2ea7b4bbaaff38ae2e8
Michael <kill.coder@gmail.com>
2019-02-13 23:52:17 +1030

    feat: USB Event discovery for serial devices

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { default as SerialPortHintProducer } from './src/hint-producer'
+export { default as SerialPortHintTransformer } from './src/hint-transformer'
 export { default as SerialTransport } from './src/transport'
---
+++ src/hint-transformer.ts
@@ -0,0 +1,47 @@
+import { DiscoveryHintTransformer, Hint } from '@electricui/core'
+
+import SerialPortHintProducer from './hint-producer'
+
+const dHintTransformer = require('debug')(
+  'electricui-transport-node-serial:hint-producer',
+)
+
+/**
+ * The baudrate is optional, as they may use a transformer to add several baudRate options if they wish
+ */
+interface SerialPortHintTransformerOptions {
+  usbTransportKey?: string
+  producer: SerialPortHintProducer
+}
+
+export default class SerialPortHintTransformer extends DiscoveryHintTransformer {
+  transportKey: string
+  producer: SerialPortHintProducer
+  options: SerialPortHintTransformerOptions
+
+  constructor(options: SerialPortHintTransformerOptions) {
+    super()
+
+    this.transportKey = options.usbTransportKey || 'usb'
+    this.options = options
+    this.producer = options.producer
+  }
+
+  canTransform(hint: Hint): boolean {
+    return hint.getTransportKey() === this.transportKey
+  }
+
+  transform(hint: Hint) {
+    // the transport key is from the usb
+
+    this.producer.poll()
+
+    if (hint.isAvailabilityHint()) {
+      // availability hint
+
+      return
+    }
+
+    // unavailability hint, we want to find
+  }
+}
