0a7f9e3063e8f5268e54570008c558fa868c4803
Michael Orenstein <michael@whiteshadows.me>
2020-02-17 23:15:42 +1030

    bug: Noop for perf functions in Node test environments

--- src/perf.ts
+++ src/perf.ts
@@ -1,14 +1,23 @@
 // Here we do our perf marks.
 import {} from '@electricui/build-rollup-config'
 
+// We only use these in the browser so make them noop in testing
+const perf =
+  typeof window === 'undefined'
+    ? {
+        mark: () => {},
+        measure: () => {},
+      }
+    : performance
+
 export function mark(name: string) {
-  if (__DEV__) performance.mark(name)
+  if (__DEV__) perf.mark(name)
 }
 
 export function measure(start: string) {
-  if (__DEV__) performance.measure(start, start)
+  if (__DEV__) perf.measure(start, start)
 }
 
 export function measurePair(name: string, start: string, end: string) {
-  if (__DEV__) performance.measure(name, start, end)
+  if (__DEV__) perf.measure(name, start, end)
 }
